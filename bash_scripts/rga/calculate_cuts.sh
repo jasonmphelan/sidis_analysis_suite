822afcedd8208cea262a3c73162399a6485b9f33
jasonmphelan <jphelan@mit.edu>
2026-03-10 10:53:56 -0400

    updates to runlist

---
+++ bash_scripts/rga/calculate_cuts.sh
@@ -0,0 +1,5 @@
+#!/bash
+
+./../../build/cutAnalysis/fiducials ../histograms/rga/fiducials.root 10.6 1 &
+./../../build/cutAnalysis/calcSFCuts ../histograms/rga/SF_cuts.root 10.6 1 &
+./../../build/cutAnalysis/calcVertex ../histograms/rga/vertex_cuts.root 10.6 1 &
